b7b9a739e38f35db6e1db984029f81912dccab29
Artem Matvieiev <artemmatvieiev@gmail.com>
2017-10-19 20:42:52 +0300

    changed main.js in first task

--- 1/main.js
+++ 1/main.js
@@ -1,6 +1,7 @@
-console.log('date:', new Date().toLocaleDateString('en-GB', {
-    hour: "2-digit",
-    minute: "2-digit"
-  })
-);
-console.log('run file:',process.argv[1]);
+const date = new Date().toLocaleDateString('en-GB', {
+  hour: "2-digit",
+  minute: "2-digit"
+});
+const nameFile = process.argv[1].match(/\w+.js$/g)[0];
+console.log('date:', date);
+console.log('file:', nameFile);
